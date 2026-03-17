15e6207ff15e983303c5c6b7a89f85d2180425da
Kari <rompela@breakbox.mx>
2026-03-17 00:48:26 -0600

    Cambio a landing page

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -2,14 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router } from 'react-router-dom'
 import '../styles/index.css'
-import { ThankYouPage } from './pages/ThankYouPage'
+import { LandingPage } from './pages/LandingPage'
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 
   <React.StrictMode>
     <Router>
-      <ThankYouPage />
+      <LandingPage />
     </Router>
   </React.StrictMode>,
 )
