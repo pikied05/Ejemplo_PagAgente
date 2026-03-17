698b718afb2960b1b75accfa1ee7a72b2e83c22c
Kari <rompela@breakbox.mx>
2026-03-17 01:20:16 -0600

    camnbio

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router } from 'react-router-dom'
 import '../styles/index.css'
-import App from './App'
+//import App from './App'
 import { LandingPage } from './pages/LandingPage'
 
 
