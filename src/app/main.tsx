014936203372465733019c2cd653db57ab127f25
Kari <rompela@breakbox.mx>
2026-03-17 00:57:55 -0600

    Cambio a app

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -2,14 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router } from 'react-router-dom'
 import '../styles/index.css'
-import { LandingPage } from './pages/LandingPage'
+import App from './App.tsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 
   <React.StrictMode>
     <Router>
-      <LandingPage />
+      <App />
     </Router>
   </React.StrictMode>,
 )
