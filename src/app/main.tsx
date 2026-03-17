e995e334993eae2ab29f26149c6736af0a7b79b4
Kari <rompela@breakbox.mx>
2026-03-17 01:19:52 -0600

    Landing Page

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router } from 'react-router-dom'
 import '../styles/index.css'
 import App from './App'
+import { LandingPage } from './pages/LandingPage'
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 
   <React.StrictMode>
     <Router>
-      <App />
+      <LandingPage />
     </Router>
   </React.StrictMode>,
 )
