5bce54bcd2368d65c54d8aa9cfc7768760aeff72
Kari <rompela@breakbox.mx>
2026-03-17 01:16:04 -0600

    Cambio

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,11 +2,14 @@ import {  Routes, Route } from 'react-router';
 import { LandingPage } from './pages/LandingPage';
 import { ThankYouPage } from './pages/ThankYouPage';
 
-export default function App() {
+ function App() {
+  return(
    <div className="app">
         <Routes> 
           <Route path="/" element={<LandingPage/>} />
           <Route path="/gracias" element={<ThankYouPage/>} />
         </Routes>
       </div>
+  )
 }
+export default App;
--- src/app/main.tsx
+++ src/app/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router } from 'react-router-dom'
 import '../styles/index.css'
-import App from './App.tsx'
+import App from './App'
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
